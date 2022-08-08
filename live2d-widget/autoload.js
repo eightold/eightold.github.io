96042cbc89d2ee11fdc4bd47a566a320fe8815a3
eightold <2554855@qq.com>
2022-08-08 19:36:01 +0800

    upload

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/"
+const live2d_path = "https://github.com/CrazyZhang666/live2d-widget/"
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+			//apiPath: "",
+			cdnPath: ""
 		});
 	});
 }
