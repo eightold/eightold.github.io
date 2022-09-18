272055862591a82f3c9163c2729d6dd86e98684e
eightold <2298651624@qq.com>
2022-09-18 18:07:22 +0800

    upload

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "FF9900",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "0px 0px 0px 0px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "115",
+      "top": "20",
+      "key": "c9a85b09e005475b99f746d916f6a298"
+    }
+  }
